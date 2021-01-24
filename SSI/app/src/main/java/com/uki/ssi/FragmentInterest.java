e8d51442c14a128712b756a5936b6c9dc4482535
snrndi121 <snrndi4521@gmail.com>
2021-01-24 18:47:06 +0900

    UI : Recycler View 형식 추가+Fragment

---
+++ SSI/app/src/main/java/com/uki/ssi/FragmentInterest.java
@@ -0,0 +1,31 @@
+package com.uki.ssi;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentInterest extends Fragment {
+    MainActivity activity;
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        activity = (MainActivity)getActivity();
+    }
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        activity = null;
+    }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootview = (ViewGroup)inflater.inflate(R.layout.fragment_recyclerview, container, false);
+        return rootview;
+    }
+}
--- SSI/app/src/main/java/com/uki/ssi/MainActivity.java
+++ SSI/app/src/main/java/com/uki/ssi/MainActivity.java
@@ -11,6 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    FragmentHome fragmentItem1;
+    FragmentInterest fragmentItem2;
+    FragmentGraph fragmentItem3;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,12 +23,15 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragmentItem1).commit();
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragmentItem2).commit();
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_graph:
+                    mTextMessage.setText(R.string.title_graph);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragmentItem3).commit();
                     return true;
             }
             return false;
@@ -36,10 +42,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Fragment
+        fragmentItem1 = new FragmentHome();
+        fragmentItem2 = new FragmentInterest();
+        fragmentItem3 = new FragmentGraph();
+        //
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragmentItem1).commit();
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-
 }
